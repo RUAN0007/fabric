b226bba2cdd1ff8d7a9463644fdad3f2907bf537
RUAN0007 <ruanpingcheng@gmail.com>
2018-02-08 11:47:09 +0800

    Add copy proto-gen script

--- build_peer.sh
+++ build_peer.sh
@@ -1,3 +1,4 @@
-export CGO_LDFLAGS='-L/home/ruanpingcheng/Desktop/USTORE/build/lib -lprovchain_lib -lboost_system'
-export CGO_CPPFLAGS='-I/home/ruanpingcheng/Desktop/USTORE/include -I/home/ruanpingcheng/Desktop/USTORE/build/include -std=c++11 -I/home/ruanpingcheng/Desktop/USTORE/prov_go/prov_chain/include'
+export CGO_LDFLAGS='-L/home/ruanpingcheng/Desktop/USTORE/build/lib -lprovchain_lib -lboost_system:$CGO_LDFLAGS='
+export CGO_CPPFLAGS='-I/home/ruanpingcheng/Desktop/USTORE/include -I/home/ruanpingcheng/Desktop/USTORE/build/include -I/home/ruanpingcheng/Desktop/USTORE/prov_go/prov_chain/include:$CGO_CPPFLAGS'
+cp ~/Desktop/gp/bin/protoc-gen-go build/docker/gotools/bin/protoc-gen-go
 make peer
